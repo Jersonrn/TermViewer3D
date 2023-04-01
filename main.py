ea082bad40af6ce5ce5c87bc56759c4ff333d086
Jerson Rodriguez <jersonrn@gmail.com>
2023-03-31 23:20:12 -0500

    added up to objects

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from Vector import Vector3D
+from objects import Torus
+
 if __name__ == "__main__":
-    pass
+    dona: Torus = Torus(position=Vector3D(0,0,-4),
+                        rotation=Vector3D(),
+                        up=Vector3D(0,0,1),
+                        major_res=30,
+                        minor_res=30,
+                        major_radio=1,
+                        minor_radio=0.55)
 
--- objects.py
+++ objects.py
@@ -20,12 +20,12 @@ class Object3D:
         
         self.__set_defaults(self.position, self.rotation)
 
-    def set_position(self, pos: Vector3D):
+    def set_position(self, pos: Vector3D) -> None:
         Pm: NDArray = np.array([
-            [1.0000,0.0000,0.0000,pos.x],
-            [0.0000,1.0000,0.0000,pos.y],
-            [0.0000,0.0000,1.0000,pos.z],
-            [0.0000,0.0000,0.0000,1.000],
+            [1.0000, 0.0000, 0.0000, pos.x],
+            [0.0000, 1.0000, 0.0000, pos.y],
+            [0.0000, 0.0000, 1.0000, pos.z],
+            [0.0000, 0.0000, 0.0000, 1.000],
             ])
         
         self.matrix = np.dot(Pm,self.matrix)
@@ -73,12 +73,13 @@ class Object3D:
 class Torus(Object3D):
     def __init__(self,
                  position: Vector3D = Vector3D(),
+                 rotation: Vector3D = Vector3D(),
                  up: Vector3D = Vector3D(0, 1, 0),
                  major_res: int = 10,
                  minor_res: int = 10,
                  major_radio: float = 10,
                  minor_radio: float = 3) -> None:
-        super().__init__(position, up)
+        super().__init__(position, rotation, up)
         self.major_res: int = major_res
         self.minor_res: int  = minor_res
         self.major_radio: float = major_radio
@@ -86,30 +87,67 @@ class Torus(Object3D):
         self.vertices: NDArray
         self.normales: NDArray
         self.update_mesh(self.minor_res, self.major_res,
-                         self.minor_radio, self.major_radio)
+                         self.minor_radio, self.major_radio,
+                         self.up)
 
     def update_mesh(self, minor_res: int, major_res: int,
-                    minor_radio: float, major_radio: float) -> None:
+                    minor_radio: float, major_radio: float,
+                    up: Vector3D) -> None:
         i: NDArray = np.linspace(start=0, stop=360, num=minor_res, endpoint=False)
         j: NDArray = np.linspace(start=0, stop=360, num=major_res, endpoint=False)
         alpha,phi = np.meshgrid(i,j)
         
-        vertices: NDArray = np.array([
-            [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
-            [minor_radio * np.cos(np.deg2rad(alpha))],
-            [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * -np.sin(np.deg2rad(phi))]
-            ]).reshape(3,-1)
+        #X_UP
+        if up.x == 1 and up.y == 0 and up.z == 0:
+            vertices: NDArray = np.array([
+                [minor_radio * np.sin(np.deg2rad(alpha))],
+                [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
+                [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * np.sin(np.deg2rad(phi))]
+                ]).reshape(3,-1)
+            
+            normales: NDArray = np.array([
+                [minor_radio * np.sin(np.deg2rad(alpha))],
+                [(minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
+                [(minor_radio * np.cos(np.deg2rad(alpha))) * np.sin(np.deg2rad(phi))]
+                ]).reshape(3,-1)
+            
+            self.vertices: NDArray = vertices
+            self.normales: NDArray = normales
         
-        normales: NDArray = np.array([
-            [(minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
-            [minor_radio * np.cos(np.deg2rad(alpha))],
-            [(minor_radio * np.cos(np.deg2rad(alpha))) * -np.sin(np.deg2rad(phi))]
-            ]).reshape(3,-1)
+        #Y_UP
+        elif up.x == 0 and up.y == 1 and up.z == 0:
+            vertices: NDArray = np.array([
+                [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
+                [minor_radio * np.cos(np.deg2rad(alpha))],
+                [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * -np.sin(np.deg2rad(phi))]
+                ]).reshape(3,-1)
+            
+            normales: NDArray = np.array([
+                [(minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
+                [minor_radio * np.cos(np.deg2rad(alpha))],
+                [(minor_radio * np.cos(np.deg2rad(alpha))) * -np.sin(np.deg2rad(phi))]
+                ]).reshape(3,-1)
+         
+            self.vertices: NDArray = vertices
+            self.normales: NDArray = normales
         
-        self.vertices: NDArray = vertices
-        self.normales: NDArray = normales
+        #Z_UP
+        elif up.x == 0 and up.y == 0 and up.z == 1:
+            vertices: NDArray = np.array([
+                [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
+                [(major_radio + minor_radio * np.cos(np.deg2rad(alpha))) * np.sin(np.deg2rad(phi))],
+                [minor_radio * np.sin(alpha)],
+                ]).reshape(3,-1)
+            
+            normales: NDArray = np.array([
+                [(minor_radio * np.cos(np.deg2rad(alpha))) * np.cos(np.deg2rad(phi))],
+                [(minor_radio * np.cos(np.deg2rad(alpha))) * np.sin(np.deg2rad(phi))],
+                [minor_radio * np.sin(np.deg2rad(alpha))],
+                ]).reshape(3,-1)
+            
+            self.vertices: NDArray = vertices
+            self.normales: NDArray = normales
+            
+        else:
+            raise Exception(ValueError)
 
-
-dona: Torus = Torus(Vector3D(2,3,1))
-print(dona.matrix)
-# dona.rotate_x(90)
